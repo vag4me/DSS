9e3d7ca00bd5aabd4472be7a1b35f12c6a894a84
Evangelos Konstantinidis <93388918+vag4me@users.noreply.github.com>
2024-04-05 13:41:47 +0300

    Update Model_training.py

--- Model_training.py
+++ Model_training.py
@@ -1,28 +1,32 @@
 import pandas as pd
-import numpy as np 
-import warnings
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
+from sklearn.metrics import accuracy_score
 import joblib
 
+# Step 1: Load the CSV file
+data = pd.read_csv("C:/Users/user/Desktop/Ionio/DSS/Train.csv")
 
-def train_model():
-    trained_model_file = ("C:/Users/user/Desktop/Ionio/DSS/trained_rf_model.pkl")
-    data = pd.read_csv("C:/Users/user/Desktop/Ionio/DSS/Preprocessed_Train.csv")
+data = pd.get_dummies(data)
 
-    # Separate features (X) and target variable (y)
-    X = data[data.columns[:-1]] 
-    y = data[data.columns[-1]] 
+# Split the data into training and testing sets
+X = data[data.columns[:-1]] # Features
+y = data[data.columns[-1]] # Target variable
 
-    
-    # Create and train Random Forest classifier
-    rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-    rf_classifier.fit(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    joblib.dump(rf_classifier, trained_model_file)
-   
+# Step 4: Choose a Model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Step 5: Train the Model
+model.fit(X_train, y_train)
+
+# Step 6: Evaluate the Model
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+joblib.dump(model, 'Trained_model.pkl')
 
 
-warnings.simplefilter(action='ignore', category=FutureWarning)
-train_model()
