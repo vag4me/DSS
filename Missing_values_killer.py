a7256da1e98956ac4306b9f58c573f0c90c0ce3a
Evangelos Konstantinidis <93388918+vag4me@users.noreply.github.com>
2024-04-05 13:17:37 +0300

    Update Missing_values_killer.py

--- Missing_values_killer.py
+++ Missing_values_killer.py
@@ -2,18 +2,24 @@ import pandas as pd
 import numpy as np 
 import warnings
 
-def check_missing_values(csv_file):
-    df = pd.read_csv(csv_file) #pernao to excel sto df
-    df['travel_with'].fillna('Alone', inplace = True) #Βαζει alone στις χαμενες τιμες της μεταβλητης travel_with
-    mean_total_female = df['total_female'].mean()  #Υπολογιζει τον μεσο ορο της μεταβλατης total_female
-    df['total_female'].fillna('mean_total_female', inplace = True) #Γεμιζει τις missing_values με τον μερο ορο 
-    mean_total_male = df.total_male.mean()  #Ιδια διαδικασια για τους αντρας ομως
-    df['total_male'].fillna('mena_total_male' , inplace = True)
-    df.to_csv("C:/Users/user/Desktop/Ionio/DSS/Train.csv", index=False)
+def preprocess_and_check_missing_values(csv_file):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(csv_file) 
     
-    missing_values = df.isnull().sum()
-    print(missing_values[missing_values > 0])
-
+    # Handle missing values
+    df['travel_with'].fillna('Alone', inplace=True) 
+    mean_total_female = df['total_female'].mean()  
+    df['total_female'].fillna(mean_total_female, inplace=True) 
+    mean_total_male = df['total_male'].mean()  
+    df['total_male'].fillna(mean_total_male, inplace=True)
+    
+    # Perform one-hot encoding
+    df = pd.get_dummies(df)
+    
+    # Save the preprocessed DataFrame back to a CSV file
+    df.to_csv("C:/Users/user/Desktop/Ionio/DSS/Preprocessed_Train.csv", index=False)
+    
+    
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
-check_missing_values('C:/Users/user/Desktop/Ionio/DSS/Train.csv')
+preprocess_and_check_missing_values('C:/Users/user/Desktop/Ionio/DSS/Train.csv')
